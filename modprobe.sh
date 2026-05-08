5381b7961da65cdc9218fb3a9de1a42b882994b8
Julian Brost <julian@0x4a42.net>
2026-05-08 22:24:16 +0200

    Include project name in kernel log
    This makes it more obvious that the messages aren't coming from the standard
    modprobe command.

--- modprobe.sh
+++ modprobe.sh
@@ -3,4 +3,4 @@
 # SPDX-FileCopyrightText: 2026 Julian Brost <julian@0x4a42.net>
 # SPDX-License-Identifier: MIT
 
-echo "<3>modprobe: not loading module: modprobe $*" > /dev/kmsg
+echo "<3>modprobe (no-kmod-autoload): not loading module: modprobe $*" > /dev/kmsg
