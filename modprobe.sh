e52c0b9b0957d762b2b0725dd32727427ae07661
Julian Brost <julian@0x4a42.net>
2026-05-08 22:36:22 +0200

    Properly escape modprobe arguments

--- modprobe.sh
+++ modprobe.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # SPDX-FileCopyrightText: 2026 Julian Brost <julian@0x4a42.net>
 # SPDX-License-Identifier: MIT
 
-echo "<3>modprobe (no-kmod-autoload): not loading module: modprobe $*" > /dev/kmsg
+cmd="modprobe$(printf ' %q' "$@")"
+echo "<3>modprobe (no-kmod-autoload): not loading module: $cmd" > /dev/kmsg
